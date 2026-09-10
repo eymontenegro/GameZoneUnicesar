73d73d6ea208b80bf9053f12c9caef45eeb879aa
yanelis1612 <ypatriciagonzalez@unicesar.edu.co>
2026-09-10 00:05:45 -0500

    feat: implement ProductRepository load method

--- src/main/java/com/gamezone/persistence/ProductRepository.java
+++ src/main/java/com/gamezone/persistence/ProductRepository.java
@@ -8,6 +8,10 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.File;
 
 
     
@@ -50,8 +54,51 @@ public class ProductRepository {
       throw new RuntimeException("Error saving products: " + e.getMessage(), e);
     }
 }
+    private Product csvLineToProduct(String line) {
+    String[] parts = line.split(",");
+    String type = parts[0];
+    String id = parts[1];
+    String title = parts[2];
+    double price = Double.parseDouble(parts[3]);
+    int stock = Integer.parseInt(parts[4]);
+    
+    
+        if (type.equals("VIDEOGAME")) {
+        String platform = parts[5];
+        String genre = parts[6];
+        String ageRating = parts[7];
+        return new VideoGame(platform, genre, ageRating, id, title, price, stock);
+
+    } else if (type.equals("CONSOLE")) {
+     String brand = parts[5];
+        String model = parts[6];
+        String generation = parts[7];
+        return new Console(brand, model, generation, id, title, price, stock); 
+        
+    } else {
+        throw new IllegalArgumentException("Unknown product type: " + type);
+    }
+ }
+    public List<Product> load() {
+    List<Product> products = new ArrayList<>();
+    File file = new File("data/products.csv");
+
+    if (!file.exists()) {
+        return products; 
+    }
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        
+        String line;
+        while ((line = reader.readLine()) != null) {
+            products.add(csvLineToProduct(line));
+        }
+    } catch (IOException e) {
+        throw new RuntimeException("Error loading products: " + e.getMessage(), e);
+    }
+
+    return products;
+}
 }
 
   
-
-
